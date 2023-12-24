c17720b3e881c84fdfa357baff23d2ae662167ec
gustavodiasag <gustavodias.aguiar1@gmail.com>
2023-12-24 19:39:44 -0300

    refac: public interface functions

--- lib/interpreter.ml
+++ lib/interpreter.ml
@@ -1,10 +1,5 @@
 open Ast
 
-let parse s =
-  let lexbuf = Lexing.from_string s in
-  Parser.prog Lexer.read lexbuf
-;;
-
 (** [is_value e] is whether [e] is a value. *)
 let is_value (e : expr) : bool =
   match e with
@@ -36,15 +31,15 @@ let rec step (e : expr) : expr =
   | Var _ -> failwith "Unbound variable"
   | Binop (bop, e1, e2) when is_value e1 && is_value e2 ->
       step_bop bop e1 e2
-  | Binop (bop e1, e2) when is_value e1 -> Binop (bop, e1, step e2)
+  | Binop (bop, e1, e2) when is_value e1 -> Binop (bop, e1, step e2)
   | Binop (bop, e1, e2) -> Binop (bop, step e1, e2)
   | Let (x, e1, e2) when is_value e1 -> sub e2 e1 x
   | Let (x, e1, e2) -> Let (x, step e1, e2)
   | If (Bool true, e2, _) -> e2
   | If (Bool false, _, e3) -> e3
-  | If (e1, e2, e3) -> If (step e1, e2, e3)
   | If (Int _, _, _) -> failwith "Guard of 'if' must have type bool"
-
+  | If (e1, e2, e3) -> If (step e1, e2, e3)
+  
 (** [step_bop bop v1 v2] implements the primitive operation [v1 bop v2].
     Requires: [v1] and [v2] are both values. *)
 and step_bop bop e1 e2 =
@@ -52,7 +47,7 @@ and step_bop bop e1 e2 =
   | Add, Int a, Int b -> Int (a + b)
   | Mult, Int a, Int b -> Int (a * b)
   | Leq, Int a, Int b -> Bool (a <= b)
-  | _ failwith "Operator and operand type mismatch"
+  | _ -> failwith "Operator and operand type mismatch"
 ;;
 
 (** [eval_small e] is the multistep relation. That is, keep applying [step] until a
@@ -69,15 +64,15 @@ let rec eval_big (e : expr) : expr =
   | Var _ -> failwith "Unbound variable"
   | Binop (bop, e1, e2) -> eval_bop bop e1 e2
   | Let (x, e1, e2) -> sub e2 (eval_big e1) x |> eval_big
-  | If (e1, e2, e3) eval_if e1 e2 e3
+  | If (e1, e2, e3) -> eval_if e1 e2 e3
 
 (** [eval_bop bop e1 e2] is the [e] such that [e1 bop e2 = e]. *)
 and eval_bop bop e1 e2 =
   match bop, eval_big e1, eval_big e2 with
   | Add, Int a, Int b -> Int (a + b)
   | Mult, Int a, Int b -> Int (a * b)
-  | Leq, Bool a, Bool b -> Bool (a <= b)
-  | _ failwith "Operator and operand type mismatch"
+  | Leq, Int a, Int b -> Bool (a <= b)
+  | _ -> failwith "Operator and operand type mismatch"
 
 (** [eval_if e1 e2 e3] is the [e] such that [if e1 then e2 else e3 = e]. *)
 and eval_if e1 e2 e3 =
@@ -86,3 +81,20 @@ and eval_if e1 e2 e3 =
   | Bool false -> eval_big e3
   | _ -> failwith "Guard of 'if' must have type bool"
 ;;
+
+(** [parse s] parses [s] into an AST. *)
+let parse s =
+  let lexbuf = Lexing.from_string s in
+  Parser.prog Lexer.read lexbuf
+;;
+
+let interpret_small s =
+  let e = parse s in
+  eval_small e
+;;
+
+let interpret_big s =
+  let e = parse s in
+  eval_big e
+;;
+
--- lib/interpreter.mli
+++ lib/interpreter.mli
@@ -1,1 +1,7 @@
-val parse : string -> Ast.expr
+val interpret_small : string -> Ast.expr
+(** [interpret_small s] interprets [s] by parsing, type-checking, and
+    evaluating it with the small-step model. *)
+    
+val interpret_big : string -> Ast.expr
+(** [interpret_big s] interprets [s] by parsing, type-checking, and evaluating
+    it with the big-step model. *)
