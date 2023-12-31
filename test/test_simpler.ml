6b1997888cc0c61d6ce5560c545134fe6fd87e65
gustavodiasag <gustavodias.aguiar1@gmail.com>
2023-12-31 19:39:43 -0300

    test: separate numeric and boolean expression tests

--- test/test_simpler.ml
+++ test/test_simpler.ml
@@ -1,68 +1,77 @@
 open OUnit2
 open Simpler
+open Ast
 
 let interpret = Interpreter.interpret
 
-open Ast
-
 let test_expr name ~e ~s =
   name >:: (fun _ -> assert_equal ~printer:show_expr e (interpret s))
-;;  
+;;
+
+let test_int_expr name ~e ~s =
+  name >:: (fun _ ->
+    assert_equal ~printer:show_expr (Int e) (interpret s))
+;;
+
+let test_bool_expr name ~e ~s =
+  name >:: (fun _ ->
+    assert_equal ~printer:show_expr (Bool e) (interpret s))
+;;
 
 let test_value =
-  [ test_expr "int" ~e:(Int 22) ~s:"22" 
-  ; test_expr "true" ~e:(Bool true) ~s:"true"
+  [ test_int_expr "int" ~e:22 ~s:"22" 
+  ; test_bool_expr "true" ~e:true ~s:"true"
   ]
 
 let test_add =
-  [ test_expr "add" ~e:(Int 22) ~s:"11 + 11"
-  ; test_expr "add_prec" ~e:(Int 22) ~s:"(10 + 1) + (5 + 6)"
-  ; test_expr "add_assoc" ~e:(Int 22) ~s:"10 + (1 + (5 + 6))"
+  [ test_int_expr "add" ~e:22 ~s:"11 + 11"
+  ; test_int_expr "add_prec" ~e:22 ~s:"(10 + 1) + (5 + 6)"
+  ; test_int_expr "add_assoc" ~e:22 ~s:"10 + (1 + (5 + 6))"
   ]
 
 let test_mult =
-  [ test_expr "mult" ~e:(Int 22) ~s:"2 * 11"
-  ; test_expr "mult_prec" ~e:(Int 22) ~s:"2 + 2 * 10"
-  ; test_expr "mult_prec2" ~e:(Int 14) ~s:"2 * 2 + 10"
-  ; test_expr "mult_assoc" ~e:(Int 40) ~s:"2 * 2 * 10"
+  [ test_int_expr "mult" ~e:22 ~s:"2 * 11"
+  ; test_int_expr "mult_prec" ~e:22 ~s:"2 + 2 * 10"
+  ; test_int_expr "mult_prec2" ~e:14 ~s:"2 * 2 + 10"
+  ; test_int_expr "mult_assoc" ~e:40 ~s:"2 * 2 * 10"
   ]
 
 let test_lt =
-  [ test_expr "lt_true" ~e:(Bool true) ~s:"50 < 100"
-  ; test_expr "lt_false" ~e:(Bool false) ~s:"7 < 4"
-  ; test_expr "lt_add" ~e:(Bool false) ~s:"if (3 + 2) < 1 then true else false"
+  [ test_bool_expr "lt_true" ~e:true ~s:"50 < 100"
+  ; test_bool_expr "lt_false" ~e:false ~s:"7 < 4"
+  ; test_bool_expr "lt_add" ~e:false ~s:"if (3 + 2) < 1 then true else false"
   ]
 
 let test_gt =
-  [ test_expr "gt_true" ~e:(Bool true) ~s:"100 > 50"
-  ; test_expr "gt_false" ~e:(Bool false) ~s:"4 > 7"
+  [ test_bool_expr "gt_true" ~e:true ~s:"100 > 50"
+  ; test_bool_expr "gt_false" ~e:false ~s:"4 > 7"
   ]
 
 let test_eq =
-  [ test_expr "eq_int" ~e:(Bool true) ~s:"8 = 8"
-  ; test_expr "eq_bool" ~e:(Bool false) ~s: "false = true"
-  ; test_expr "eq_cond" ~e:(Int 6) ~s:"if 1 = 2 then 3 else 6"
+  [ test_bool_expr "eq_int" ~e:true ~s:"8 = 8"
+  ; test_bool_expr "eq_bool" ~e:false ~s: "false = true"
+  ; test_int_expr "eq_cond" ~e:6 ~s:"if 1 = 2 then 3 else 6"
   ]
 
 let test_leq =
-  [ test_expr "leq_true" ~e:(Bool true) ~s:"1 <= 1" ]
+  [ test_bool_expr "leq_true" ~e:true ~s:"1 <= 1" ]
 
 let test_geq =
-  [ test_expr "geq_true" ~e:(Bool true) ~s:"0 >= -1"]
+  [ test_bool_expr "geq_true" ~e:true ~s:"0 >= -1"]
 
 let test_cond =
-  [ test_expr "if" ~e:(Int 22) ~s:"if true then 22 else 0"
-  ; test_expr "if_leq" ~e:(Int 22) ~s:"if 1 + 2 <= 3 + 4 then 22 else 0"
-  ; test_expr "if_let" 
-      ~e:(Int 22) 
+  [ test_int_expr "if" ~e:22 ~s:"if true then 22 else 0"
+  ; test_int_expr "if_leq" ~e:22 ~s:"if 1 + 2 <= 3 + 4 then 22 else 0"
+  ; test_int_expr "if_let" 
+      ~e:22 
       ~s:"if 1 + 2 <= 3 * 4 then let x = 22 in x else 0"
   ]
 
 let test_let =
-  [ test_expr "let_id" ~e:(Int 22) ~s:"let x = 22 in x"
-  ; test_expr "lets" ~e:(Int 22) ~s:"let x = 0 in let x = 22 in x"
-  ; test_expr "let_if"
-      ~e:(Int 22)
+  [ test_int_expr "let_id" ~e:22 ~s:"let x = 22 in x"
+  ; test_int_expr "lets" ~e:22 ~s:"let x = 0 in let x = 22 in x"
+  ; test_int_expr "let_if"
+      ~e:22
       ~s:"let x = 1 + 2 <= 3 * 4 in if x then 22 else 0"
   ; test_expr "let_fun" ~e:(Fun ("x", Var "x")) ~s:"let x = (fun x -> x) in x"
   ]
